f3b0c807387f2f771f3e221a0c25780229e2f84c
Max Gerlach <mgerlach@klick.com>
2014-06-09 08:57:31 -0400

    Initial Commit

---
+++ KlickTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KlickTimer
+//
+//  Created by Max Gerlach on 2014-06-09.
+//  Copyright (c) 2014 Klick Health. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
